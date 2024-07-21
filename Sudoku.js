70c9a40016815919dc3c363a0df8824641f9b808
PUNEET RATHI <115484581+puneet21rathi@users.noreply.github.com>
2024-07-21 22:10:02 +0530

    Update Sudoku.js

--- Sudoku.js
+++ Sudoku.js
@@ -1,3 +1,4 @@
+
 "use strict";
 
 var EASY_PUZZLE = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--";
@@ -169,3 +170,4 @@ var SudokuSolver = function (testable) {
 
   return solver;
 }(TESTABLE);
+
